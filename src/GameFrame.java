14b273ba5e42820140a3beddde86ea024705efa6
Johnny <jmccauley117@gmail.com>
2020-11-14 17:46:18 +0000

    Start of project

---
+++ src/GameFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+/**
+ * Created By Jonathon on 14/11/2020
+ * Update Comments About Program Here
+ **/
+public class GameFrame extends JFrame {
+}
---
+++ src/GamePanel.java
@@ -0,0 +1,6 @@
+/**
+ * Created By Jonathon on 14/11/2020
+ * Update Comments About Program Here
+ **/
+public class GamePanel  {
+}
